cb9caae7530cfb468eb0cef665234aab7d844e35
taiprovip3 <taito1doraemon@gmail.com>
2025-07-23 00:12:46 +0700

    init/skeleton

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import fullNameImg from './assets/images/fullname.png';
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col border border-red-500">
+        <header className="border-4 border-purple-500 flex justify-between items-center">
+          abc
+        </header>
+
+        <div className="flex flex-1">
+        </div>
+
+        <footer className="bg-gray-800 text-white p-4 text-center">
+          <p>&copy; 2023 Tên công ty của bạn. Bảo lưu mọi quyền.</p>
+        </footer>
+      </div>      
+    </>
+  )
+}
+
+export default App;
